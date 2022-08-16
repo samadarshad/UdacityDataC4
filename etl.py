aa7225c2168125582080bbc5a5965bcb564c2434
Samad Arshad <samad.arshad@ev.energy>
2022-08-16 22:08:38 +0100

    Local ETL for songs_data

--- etl.py
+++ etl.py
@@ -6,39 +6,31 @@ from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
 
-config = configparser.ConfigParser()
-config.read('dl.cfg')
-
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
-
-
 def create_spark_session():
     spark = SparkSession \
         .builder \
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
         .getOrCreate()
     return spark
 
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = "./data/song_data/*/*/*"
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = df.select("song_id", "title", "artist_id", "year", "duration")
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.parquet("./output/songs/", partitionBy=['year', 'artist_id'], mode='overwrite')
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = df.select("artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude")
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet("./output/artists", mode='overwrite')
 
 
 def process_log_data(spark, input_data, output_data):
@@ -83,7 +75,7 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
+    input_data = "./data/"
     output_data = ""
     
     process_song_data(spark, input_data, output_data)    
